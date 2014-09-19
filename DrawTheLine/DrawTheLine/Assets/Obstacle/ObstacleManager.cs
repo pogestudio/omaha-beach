711fc743a7c3854d24220605292b4b4f58e03873
CA Ewerbring <caewerbring@gmail.com>
2014-09-19 03:00:45 +0200

    Fixed block spawning:

---
+++ DrawTheLine/DrawTheLine/Assets/Obstacle/ObstacleManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ObstacleManager : MonoBehaviour
+{
+
+
+		public Vector3 startPosition;
+		public int numberOfObjects;
+		public float recycleOffset;
+		private Vector3 nextPosition;
+	
+		public Transform prefab;
+		private Queue<Transform> objectQueue;
+	
+		void Start ()
+		{
+				objectQueue = new Queue<Transform> (numberOfObjects);
+				for (int i = 0; i < numberOfObjects; i++) {
+						objectQueue.Enqueue ((Transform)Instantiate (prefab));
+				}
+				nextPosition = startPosition;
+				for (int i = 0; i < numberOfObjects; i++) {
+						Recycle ();
+				}
+		}
+	
+		void Update ()
+		{
+				if (objectQueue.Peek ().localPosition.y + recycleOffset < PlayerScript.distanceTraveled) {
+						Recycle ();
+				}
+		}
+	
+		private void Recycle ()
+		{
+				Transform obstacle = objectQueue.Dequeue ();
+				obstacle.localPosition = nextPosition;
+				nextPosition.y += obstacle.localScale.y;
+				objectQueue.Enqueue (obstacle);
+		}
+}
